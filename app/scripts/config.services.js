94113c3e147a43928c9deafeb2d83e2ddcac8bd6
el-sonny <sonny@spaceshiplabs.com>
2016-03-22 14:59:19 -0500

    route api correctly

--- app/scripts/config.services.js
+++ app/scripts/config.services.js
@@ -1,6 +1,6 @@
 angular
   .module('deudamxApp')
   .config(function(RestangularProvider) {
-    //RestangularProvider.setBaseUrl('http://deudamx-api.herokuapp.com/');
-    RestangularProvider.setBaseUrl('http://localhost:1337/');
+    RestangularProvider.setBaseUrl('http://deudamx-api.herokuapp.com/');
+    //RestangularProvider.setBaseUrl('http://localhost:1337/');
   });
